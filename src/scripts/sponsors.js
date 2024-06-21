a2fe20dd2c53b336766a4405a8af2b0163160b72
William Harrison <william@williamdavidharrison.com.au>
2024-06-21 20:23:22 +0800

    update sponsors loading, add percentage

--- src/scripts/sponsors.js
+++ src/scripts/sponsors.js
@@ -1,62 +1,88 @@
 const sponsorsList = document.getElementById("sponsors-list");
-const sponsorLoader = document.getElementById("sponsors-loader");
-const sponsorLoadingError = document.getElementById("loading-error");
+const sponsorsLoader = document.getElementById("sponsors-loader");
+const sponsorsLoaderPercentage = document.getElementById("sponsors-loader-percentage");
+const sponsorsLoadingError = document.getElementById("loading-error");
 const noSponsors = document.getElementById("no-sponsors");
 
-async function fetchSponsors() {
-    await fetch("https://ghs.vercel.app/v3/sponsors/wdhdev").then((res) => res.json()).then(async (data) => {
-        const sponsors = data.sponsors.current;
+fetch("https://ghs.vercel.app/v3/sponsors/wdhdev").then((res) => res.json()).then(async (data) => {
+    let sponsors = [];
 
-        if(sponsors == null || sponsors.length === 0) {
-            sponsorLoader.classList.add("hidden");
-            noSponsors.classList.remove("hidden");
-            return;
+    if(data.sponsors.current !== null) sponsors = [...data.sponsors.current];
+
+    if(!sponsors.length) {
+        sponsorsLoader.classList.add("hidden");
+        noSponsors.classList.remove("hidden");
+        return;
+    }
+
+    const avatars = [];
+
+    await new Promise((resolve) => {
+        let loaded = 0;
+
+        const updatePercentage = async () => {
+            const percentage = Math.round((loaded / sponsors.length) * 100);
+            sponsorsLoaderPercentage.textContent = `Loading... ${percentage}%`;
         }
 
-        await sponsors.forEach(async (sponsor) => {
-            // <div class="p-2 bg-[#11111199] rounded-md">
-            let div = document.createElement("div");
-            sponsorsList.appendChild(div);
-            // <li>
-            let li = document.createElement("li");
-            div.appendChild(li);
-            // <div class="flex items-center gap-x-6">
-            let div2 = document.createElement("div");
-            li.appendChild(div2);
-            // <img class="h-18 w-32 rounded-md">
-            let img = document.createElement("img");
-            div2.appendChild(img);
-            // <div>
-            let div3 = document.createElement("div");
-            div2.appendChild(div3);
-            // <h3 class="text-base font-semibold leading-7 tracking-tight text-pink-500 hover:text-pink-600">
-            let h3 = document.createElement("h3");
-            div2.appendChild(h3);
-            // <a href="https://github.com/${sponsor.username" target="_blank">${sponsor.username}</a>
-            let a = document.createElement("a");
-            h3.appendChild(a);
+        for(const sponsor of sponsors) {
+            const image = new Image();
+            image.src = `https://avatars.githubusercontent.com/${sponsor.username}`;
+            avatars.push(image);
+        }
 
-            div.classList.add("p-2", "bg-[#11111199]", "rounded-md");
-
-            div2.classList.add("flex", "items-center", "gap-x-2");
-
-            img.classList.add("h-16", "rounded-md");
-            img.src = `https://avatars.githubusercontent.com/${sponsor.username}`;
-            img.alt = sponsor.username;
-            img.draggable = false;
-
-            h3.classList.add("text-base", "font-semibold", "leading-7", "tracking-tight", "text-pink-500", "hover:text-pink-600");
-
-            a.href = `https://github.com/${sponsor.username}`;
-            a.target = "_blank";
-            a.textContent = sponsor.username;
+        avatars.forEach(async (image) => {
+            if(image.complete) {
+                loaded++;
+                updatePercentage();
+            } else {
+                image.addEventListener("load", async () => {
+                    loaded++;
+                    updatePercentage();
+                    if(loaded === avatars.length) resolve();
+                })
+            }
         })
 
-        sponsorLoader.classList.add("hidden");
-        sponsorsList.classList.remove("hidden");
-    }).catch(err => {
-        console.error("Failed to load sponsors", err)
-        sponsorLoader.classList.add("hidden");
-        sponsorLoadingError.classList.remove("hidden");
+        if(loaded === avatars.length) resolve();
     })
-}
+
+    sponsors.forEach(async (sponsor) => {
+        let div = document.createElement("div");
+        sponsorsList.appendChild(div);
+        let li = document.createElement("li");
+        div.appendChild(li);
+        let div2 = document.createElement("div");
+        li.appendChild(div2);
+        let img = document.createElement("img");
+        div2.appendChild(img);
+        let div3 = document.createElement("div");
+        div2.appendChild(div3);
+        let h3 = document.createElement("h3");
+        div2.appendChild(h3);
+        let a = document.createElement("a");
+        h3.appendChild(a);
+
+        div.classList.add("p-2", "bg-[#11111199]", "rounded-md");
+
+        div2.classList.add("flex", "items-center", "gap-x-2");
+
+        img.classList.add("h-16", "rounded-md");
+        img.src = avatars[sponsors.indexOf(sponsor)].src;
+        img.alt = sponsor.username;
+        img.draggable = false;
+
+        h3.classList.add("text-base", "font-semibold", "leading-7", "tracking-tight", "text-pink-500", "hover:text-pink-600");
+
+        a.href = `https://github.com/${sponsor.username}`;
+        a.target = "_blank";
+        a.textContent = sponsor.username;
+    })
+
+    sponsorsLoader.classList.add("hidden");
+    sponsorsList.classList.remove("hidden");
+}).catch(err => {
+    console.error("Failed to load sponsors", err)
+    sponsorsLoader.classList.add("hidden");
+    sponsorsLoadingError.classList.remove("hidden");
+})
