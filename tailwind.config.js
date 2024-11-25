5073dc3cdd73712f0e2a18003e9d1176d5160004
William Harrison <87287585+wdhdev@users.noreply.github.com>
2024-11-25 17:13:29 +0800

    deploy ads + move css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
         extend: {
             backgroundImage: {
-                main: "url('../assets/background.jpg')" // Uses ../ as the build output is in the dist/ directory
+                main: "url('../../assets/background.jpg')" // Uses ../../ as the build output is in the dist/css directory
             }
         }
     },
